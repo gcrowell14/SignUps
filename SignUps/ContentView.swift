e3b83c4bd98faedb9da7aadf927edd86d9d78ccb
Grace Crowell <gmcrow20@gmail.com>
2023-04-15 15:54:40 -0400

    Initial Commit

---
+++ SignUps/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SignUps
+//
+//  Created by Grace Crowell on 4/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignUps/SignUpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpsApp.swift
+//  SignUps
+//
+//  Created by Grace Crowell on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SignUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
